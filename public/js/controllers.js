ca59f88c89c1d5820420a308ef2465b5feed04d1
andrewcbass <andrew.c.bass@gmail.com>
2016-03-22 13:01:31 -0700

    background image

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -70,7 +70,6 @@ app.controller("homeCtrl", function($scope, $state, ReservationService) {
 
   $scope.showOrEdit = true;
   $scope.editReservation = function(reso) {
-    console.log(("reso:", reso));
 
     $scope.showOrEdit = !$scope.showOrEdit;
     reso.time = new Date(reso.time);
